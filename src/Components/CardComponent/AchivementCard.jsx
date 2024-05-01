f9866ba63768c20d0c6e351a8071997a162c2ef7
gautamProject <gmgautam2374@gmail.com>
2024-05-01 20:27:45 +0530

    nothing

--- src/Components/CardComponent/AchivementCard.jsx
+++ src/Components/CardComponent/AchivementCard.jsx
@@ -4,7 +4,7 @@ const AchivementCard = ({ data }) => {
     // <Box>
       <Box
         sx={{
-          width:{xs:"400px",xl:"400px"},
+          width:{xs:"400px",xl:"400px",sm:"400px",md:"400px"},
           height: {xs:"160px",sm:"150px",md:"150px",lg:"150px",xl:"150px"},
           borderRadius: "15px",
           display: "flex",
